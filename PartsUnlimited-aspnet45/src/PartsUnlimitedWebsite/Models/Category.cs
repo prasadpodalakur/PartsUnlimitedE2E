218b8e36d1e4d6fef594cb8385310dba6f41f2a7
prasadpodalakur <prasad.podalakur@gmail.com>
2020-12-22 03:26:11 +0000

    commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class Category
     {
-            //Logging categories
+            //Logging categories12
         public int CategoryId { get; set; }
 
         [Required]
