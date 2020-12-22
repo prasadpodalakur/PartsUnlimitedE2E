6ebbf80ee85870bf066c5c61c88a186f4bb7b235
prasadpodalakur <prasad.podalakur@gmail.com>
2020-12-22 03:13:56 +0000

    new change

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-
+//this is my first code for CGI demo
 namespace PartsUnlimited.Models
 {
     public class Category
